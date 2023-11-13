f8886ee028575946ce79555dd3abc6e90917ad93
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-11-14 00:08:21 +0900

    update:入力欄が空白の時にエラー表示

--- js/addTodo.js
+++ js/addTodo.js
@@ -33,21 +33,24 @@ export function AddTodo() {
 
   const createBtn = document.querySelector('.modal__create__btn');
   createBtn.addEventListener('click', function () {
-    const addTodoItem = document.querySelector('#content-create');
     let haveError = false;
+    const addTodoItem = document.querySelector('#content-create');
     // 入力した文字数を検証
     const validateItems = document.querySelectorAll('.maxlength');
     validateItems.forEach((item) => {
       const maxlength = item.getAttribute('data-maxlength');
-      if (item.value.length > maxlength) {
-        createError(item, 'ERROR：' + maxlength + '文字以内で入力');
+      if (item.value.trim() === '') {
+        createError(item, 'ERROR：入力してください');
+        haveError = true;
+      } else if (item.value.length > maxlength) {
+        createError(item, 'ERROR：' + maxlength + '文字以内で入力してください');
         haveError = true;
       } else {
         removeError(item);
       }
     });
 
-    if (!haveError && addTodoItem.value) {
+    if (haveError && addTodoItem.value) {
       const todoItem = document.createElement('li');
       todoItem.classList.add('todo__list__item');
       todoItem.innerHTML = `
